b80e08d1c4c345269c86ff10e4d0aa5690f2cb98
Jimmy Situ <web@jimmystone.cn>
2019-10-03 18:56:11 +0800

    Update ps ip for miz701n

---
+++ fpga/constraints/miz701n.xdc
@@ -0,0 +1,32 @@
+## Reset
+set_property -dict {PACKAGE_PIN M14 IOSTANDARD LVCMOS33} [get_ports cpu_resetn]
+
+# Clock
+set_property -dict { PACKAGE_PIN {H16} IOSTANDARD {LVCMOS33}} [get_ports {sys_clock}]
+
+## Use user IO as JTAG
+set_property -dict { PACKAGE_PIN F16 IOSTANDARD LVCMOS33 PULLUP {TRUE}} [get_ports { trst_n }];
+set_property -dict { PACKAGE_PIN L15 IOSTANDARD LVCMOS33 PULLUP {TRUE}} [get_ports { tck    }];
+set_property -dict { PACKAGE_PIN B19 IOSTANDARD LVCMOS33 PULLUP {TRUE}} [get_ports { tdi    }];
+set_property -dict { PACKAGE_PIN L14 IOSTANDARD LVCMOS33 PULLUP {TRUE}} [get_ports { tdo    }];
+set_property -dict { PACKAGE_PIN A20 IOSTANDARD LVCMOS33 PULLUP {TRUE}} [get_ports { tms    }];
+set_property CLOCK_DEDICATED_ROUTE {FALSE} [get_nets [get_ports {tck}]]
+set_false_path -from [get_ports { trst_n } ]
+
+
+# LEDs
+set_property -dict { PACKAGE_PIN {N15} IOSTANDARD {LVCMOS33}} [get_ports {led[0]}]
+set_property -dict { PACKAGE_PIN {N16} IOSTANDARD {LVCMOS33}} [get_ports {led[1]}]
+set_property -dict { PACKAGE_PIN {M19} IOSTANDARD {LVCMOS33}} [get_ports {led[2]}]
+set_property -dict { PACKAGE_PIN {M20} IOSTANDARD {LVCMOS33}} [get_ports {led[3]}]
+
+## Buttons
+set_property -dict { PACKAGE_PIN {M15} IOSTANDARD {LVCMOS33}} [get_ports {sw[0]}]
+
+## UART
+set_property PACKAGE_PIN {E18} [get_ports {rx}]
+set_property IOSTANDARD {LVCMOS33} [get_ports {rx}]
+set_property IOB {TRUE} [ get_cells -of_objects [ all_fanout -flat -endpoints_only [get_ports {rx}]]]
+set_property PACKAGE_PIN {G17} [get_ports {tx}]
+set_property IOSTANDARD {LVCMOS33} [get_ports {tx}]
+set_property IOB {TRUE} [ get_cells -of_objects [ all_fanin -flat -startpoints_only [get_ports {tx}]]]
--- fpga/xilinx/xlnx_ps_7/tcl/run.tcl
+++ fpga/xilinx/xlnx_ps_7/tcl/run.tcl
@@ -8,9 +8,43 @@ set_property board_part $boardName [current_project]
 
 create_ip -name processing_system7 -vendor xilinx.com -library ip -module_name $ipName
 set_property -dict [list  \
-            CONFIG.PCW_USE_S_AXI_GP0 {1} \
-            CONFIG.PCW_USE_S_AXI_HP0 {1} \
-                    ] [get_ips $ipName]
+            CONFIG.PCW_CRYSTAL_PERIPHERAL_FREQMHZ {50}          \
+            CONFIG.PCW_FPGA0_PERIPHERAL_FREQMHZ {50}            \
+            CONFIG.PCW_UIPARAM_DDR_PARTNO {MT41K256M16 RE-125}  \
+            CONFIG.PCW_USE_FABRIC_INTERRUPT {1}             \
+            CONFIG.PCW_IRQ_F2P_INTR {1}                     \
+            CONFIG.PCW_PRESET_BANK0_VOLTAGE {LVCMOS 3.3V}   \
+            CONFIG.PCW_QSPI_PERIPHERAL_ENABLE {1}           \
+            CONFIG.PCW_QSPI_GRP_SINGLE_SS_ENABLE {1}        \
+            CONFIG.PCW_QSPI_GRP_FBCLK_ENABLE {1}            \
+            CONFIG.PCW_PRESET_BANK1_VOLTAGE {LVCMOS 1.8V}   \
+            CONFIG.PCW_ENET0_PERIPHERAL_ENABLE {1}          \
+            CONFIG.PCW_ENET0_ENET0_IO {MIO 16 .. 27}        \
+            CONFIG.PCW_ENET0_GRP_MDIO_ENABLE {1}            \
+            CONFIG.PCW_ENET0_GRP_MDIO_IO {MIO 52 .. 53}     \
+            CONFIG.PCW_USB0_PERIPHERAL_ENABLE {1}           \
+            CONFIG.PCW_USB0_USB0_IO {MIO 28 .. 39}          \
+            CONFIG.PCW_SD0_PERIPHERAL_ENABLE {1}            \
+            CONFIG.PCW_SD0_SD0_IO {MIO 40 .. 45}            \
+            CONFIG.PCW_SD0_GRP_CD_ENABLE {1}                \
+            CONFIG.PCW_SD0_GRP_CD_IO {MIO 47}               \
+            CONFIG.PCW_SD1_PERIPHERAL_ENABLE {1}            \
+            CONFIG.PCW_SD1_SD1_IO {MIO 10 .. 15}            \
+            CONFIG.PCW_SD1_GRP_CD_ENABLE {1}                \
+            CONFIG.PCW_SD1_GRP_CD_IO {MIO 9}                \
+            CONFIG.PCW_SD1_GRP_WP_ENABLE {1}                \
+            CONFIG.PCW_SD1_GRP_WP_IO {MIO 0}                \
+            CONFIG.PCW_UART1_PERIPHERAL_ENABLE {1}          \
+            CONFIG.PCW_UART1_UART1_IO {MIO 48 .. 49}        \
+            CONFIG.PCW_GPIO_MIO_GPIO_ENABLE {1}             \
+            CONFIG.PCW_GPIO_MIO_GPIO_IO {MIO}               \
+            CONFIG.PCW_USB_RESET_ENABLE {1}                 \
+            CONFIG.PCW_USB0_RESET_IO {MIO 46}               \
+            CONFIG.PCW_USE_S_AXI_HP0 {1}                    \
+            CONFIG.PCW_USE_S_AXI_GP0 {1}                    \
+            CONFIG.PCW_USE_M_AXI_GP0 {0}                    \
+            CONFIG.PCW_USE_M_AXI_GP1 {0}
+] [get_ips $ipName]
 
 
 generate_target {instantiation_template} [get_files ./$ipName.srcs/sources_1/ip/$ipName/$ipName.xci]
