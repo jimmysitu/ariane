f1c85b4b5e9706008dd3d75524c7ae4fc105d600
Jimmy Situ <web@jimmystone.cn>
2020-03-31 22:08:13 +0800

    Change bin file to output dir

--- fpga/scripts/flash_zynq_qspi.tcl
+++ fpga/scripts/flash_zynq_qspi.tcl
@@ -4,7 +4,7 @@ open_hw
 connect_hw_server -url localhost:3121
 
 # Program flash
-set stdout [exec program_flash -f $designName.bin -fsbl $designName.flash/executable.elf \
+set stdout [exec program_flash -f work-fpga/ariane_zynq.bin -fsbl $designName.flash/executable.elf \
 	-flash_type qspi_single -blank_check -verify \
 	-cable type xilinx_tcf url tcp:localhost:3121]
 
